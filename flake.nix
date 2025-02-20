5afb703fc0947cb594820785df7b0295225030e5
regular <jan@lagomorph.de>
2025-02-20 16:29:45 +0100

    Add PATH= to secretsctl

--- flake.nix
+++ flake.nix
@@ -131,7 +131,7 @@
               (pkgs.writeScriptBin "secretsctl" ''
                 #!${pkgs.bash}/bin/bash
                 SECRETS_SOCKET="${cfg.socketPath}"
-                PATH="${lib.strings.makeBinPath [ coreutils-full socat ]}"
+                PATH="${lib.strings.makeBinPath (with pkgs; [ coreutils-full socat ])}"
                 ${builtins.readFile ./secretsctl.sh}
               '')
             ];
